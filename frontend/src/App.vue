6f56920ec3db47bb3d9f297abb25b12f3fa04e82
Gustavo Ramírez <gustavo81988@gmail.com>
2017-08-30 00:00:25 -0400

    Authentication & Devtools

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="container" id="app">
         <navbar></navbar>
+        <login></login>
+        <register></register>
     </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Login from './components/authentication/Login.vue';
+import Register from './components/authentication/Register.vue';
+
 export default {
     components: {
-        'navbar': Navbar
+        'navbar'  : Navbar,
+        'login'   : Login,
+        'register': Register
     }
 }
 </script>

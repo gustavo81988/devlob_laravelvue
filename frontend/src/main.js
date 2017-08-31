c731be21cc40ec5ee72f4cf07f3ed434a135a4af
Gustavo Ramírez <gustavo81988@gmail.com>
2017-08-31 12:06:20 -0400

    Restricting access to pages

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,10 +10,32 @@ Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(Auth);
 
+
+
 const router = new VueRouter({
   mode: 'history',
   routes
 }); 
+
+router.beforeEach(
+    (to, from, next) => {
+        if( to.matched.some( record => record.meta.forVisitors ) ){
+            if(Vue.auth.isAuthenticated()){
+                next({ path: '/feed' });
+            }else{
+                next();
+            }
+        } else if ( to.matched.some( record => record.meta.forAuth ) ){
+            if( !Vue.auth.isAuthenticated() ){
+                next({ path: '/login' });
+            }else{
+                next();
+            }
+        }else {
+            next();
+        }
+    }
+);
 
 new Vue({
   el: '#app',
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,9 @@
 import Login    from './components/authentication/Login.vue';
 import Register from './components/authentication/Register.vue';
+import Feed from './components/Feed.vue';
 
 export const routes = [
     {path: '/login'    , component: Login, meta : {forVisitors : true} },
-    {path: '/register' , component: Register, meta : {forVisitors : true} }
+    {path: '/register' , component: Register, meta : {forVisitors : true} },
+    {path: '/feed' , component: Feed, meta: {forAuth: true}},
 ]
